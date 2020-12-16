b2a5cadb2c1e2ed25a856d32c801814dc3939748
Arley Oliveira <arley.msn@gmail.com>
2020-12-16 18:03:05 -0300

    Cria template para extrato cfe

--- exemplo/extrato.php
+++ exemplo/extrato.php
@@ -3,6 +3,8 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $xml = __DIR__ . '/../docs/cfe.xml';
 
-$obj = new \ArleyOliveira\CFe\Extrato($xml);
+$logo = __DIR__ . '/../docs/logo.jpg';
+
+$obj = new \ArleyOliveira\CFe\Extrato($xml, $logo);
 
 echo $obj->render();
--- src/CFe/Extrato.php
+++ src/CFe/Extrato.php
@@ -1,6 +1,9 @@
 <?php
 
+
 namespace ArleyOliveira\CFe;
+
+use Dompdf\Dompdf;
 
 class Extrato
 {
@@ -22,14 +25,14 @@ class Extrato
             ? simplexml_load_string($xml)
             : simplexml_load_file($xml);
 
-        $this->logo = $logo;
-        $this->handle();
+
+        if ($logo) {
+            $type = pathinfo($logo, PATHINFO_EXTENSION);
+            $data = file_get_contents($logo);
+            $this->logo = 'data:image/' . $type . ';base64,' . base64_encode($data);
+        }
     }
 
-    private function handle()
-    {
-        $this->emitente = $this->xml->infCFe->emit;
-    }
 
     /**
      * @return string
@@ -39,18 +42,77 @@ class Extrato
      */
     private function make()
     {
+
+        $attr = '@attributes';
+        $std = new \stdClass();
+        $std->logo = $this->logo;
+        $std->emitente = $this->xml->infCFe->emit;
+
+        $std->emitente->CNPJ = vsprintf("%s%s.%s%s%s.%s%s%s/%s%s%s%s-%s%s", str_split($std->emitente->CNPJ));
+        $std->emitente->enderEmit->CEP = vsprintf("%s%s%s%s%s-%s%s%s", str_split($std->emitente->enderEmit->CEP));
+
+        $std->ide = $this->xml->infCFe->ide;
+
+        $std->chave = substr((string)$this->xml->infCFe->attributes()["Id"], 3);
+
+
+        $produtos = [];
+        if (count($this->xml->infCFe->det) > 1) {
+            foreach ($this->xml->infCFe->det as $prod) {
+                unset($prod->$attr);
+                $produtos[] = $prod;
+            }
+        } else {
+            unset($this->xml->infCFe->det->$attr);
+            $produtos[] = $this->xml->infCFe->det;
+        }
+
+        $std->produtos = $produtos;
+
+        //var_dump($std->produtos); die;
+
         $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../Resource/templates');
         $twig = new \Twig\Environment($loader);
 
-        return $twig->render('extrato.html.twig', [
-            'emitente' => $this->emitente,
-            'name' => 'Arley Oliveira'
-        ]);
+        $filter = new \Twig\TwigFilter('barcode', function ($string) {
+            $barcode = new \Com\Tecnick\Barcode\Barcode();
+            $bobj = $barcode->getBarcodeObj(
+                'QRCODE,H',                     // barcode type and additional comma-separated parameters
+                $string,          // data string to encode
+                -4,                             // bar width (use absolute or negative value as multiplication factor)
+                -4,                             // bar height (use absolute or negative value as multiplication factor)
+                'black',                        // foreground color
+                array(-2, -2, -2, -2)           // padding (use absolute or negative values as multiplication factors)
+            )->setBackgroundColor('white'); // background color
+            return $bobj->getHtmlDiv();
+        });
+
+        $twig->addFilter($filter);
+
+        return $twig->render('extrato.html.twig', (array)$std);
     }
 
     public function render()
     {
-        echo $this->make();
+        $conteudo = $this->make();
+
+        echo $conteudo;
+        die;
+
+        // instantiate and use the dompdf class
+        /*        $dompdf = new Dompdf();
+                $dompdf->loadHtml($conteudo);
+        
+                // (Optional) Setup the paper size and orientation
+                $dompdf->setPaper([0, 0, 235.00, 841.89], 'portrait');
+        
+        
+                // Render the HTML as PDF
+                $dompdf->render();
+        
+                // Output the generated PDF to Browser
+                $dompdf->stream();*/
+
     }
 
 
